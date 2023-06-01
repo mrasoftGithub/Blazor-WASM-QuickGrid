7ac42e75ab9a457c1ca09d7fb58420d7f4dbac8e
MRAsoft <info.mrasoft@gmail.com>
2023-06-01 22:59:01 +0200

    Add project files.

---
+++ Shared/Eigenaar.cs
@@ -0,0 +1,20 @@
+﻿namespace QuickGridMRA.Shared
+{
+    public class Eigenaar
+    {
+        public class EIGENAAR
+        {
+            public int? ID { get; set; } = 0;
+
+            public string? Omschrijving { get; set; } = string.Empty;
+
+            public string? Voornaam { get; set; } = string.Empty;
+
+            public string? Achternaam { get; set; } = string.Empty;
+
+            public string? Regio { get; set; } = string.Empty;
+        }
+
+    }
+
+}
